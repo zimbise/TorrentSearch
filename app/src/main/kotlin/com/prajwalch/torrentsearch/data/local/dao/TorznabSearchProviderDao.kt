087eb20f30a9c92cb510c88f91bf3d0b60fba8a8
Zim <Zimbise@gmail.com>
2025-12-09 04:44:02 -0500

    Fix corrupted TorznabSearchProviderDao

--- app/src/main/kotlin/com/prajwalch/torrentsearch/data/local/dao/TorznabSearchProviderDao.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/data/local/dao/TorznabSearchProviderDao.kt
@@ -5,13 +5,12 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
-
 import com.prajwalch.torrentsearch.data.local.entities.TorznabSearchProviderEntity
-
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TorznabSearchProviderDao {
+
     @Query("DELETE FROM torznab_search_providers")
     suspend fun deleteAll()
 
@@ -21,18 +20,18 @@ interface TorznabSearchProviderDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(searchProvider: TorznabSearchProviderEntity)
 
-    @Query("SELECT * from torznab_search_providers")
+    @Query("SELECT * FROM torznab_search_providers")
     fun observeAll(): Flow<List<TorznabSearchProviderEntity>>
 
-    @Query("SELECT * from torznab_search_providers where id=:id")
+    @Query("SELECT * FROM torznab_search_providers WHERE id = :id")
     suspend fun findById(id: String): TorznabSearchProviderEntity?
 
-    @Query("SELECT COUNT(id) from TORZNAB_SEARCH_PROVIDERS")
+    @Query("SELECT COUNT(id) FROM torznab_search_providers")
     fun observeCount(): Flow<Int>
 
     @Update
     suspend fun update(searchProvider: TorznabSearchProviderEntity)
 
-    @Query("DELETE from torznab_search_providers where id=:id")
+    @Query("DELETE FROM torznab_search_providers WHERE id = :id")
     suspend fun deleteById(id: String)
 }
